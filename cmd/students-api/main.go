5b6bfc0811fb6de9fd00be2fb58413531dc4e946
harshit3011 <harshitscience30@gmail.com>
2025-05-21 02:33:17 +0530

    demo api and server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,29 @@
- package main
+package main
 
- import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/harshit3011/students-api/internal/config"
+)
 
  func main(){
-	fmt.Println("Welcome to students api")
+	cfg:= config.MustLoad()
+	router:= http.NewServeMux()
+
+	router.HandleFunc("GET /",func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	server:=http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	err:= server.ListenAndServe()
+	if err!=nil{
+		log.Fatal("Failed to start the server: ",err.Error())
+	}
+	fmt.Println("server started")
  }
